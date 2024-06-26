4cbfde7579da1be30e1e72091cfccbd9a3388db7
TA <aaltopi@gmail.com>
2024-06-26 11:28:05 +0300

    Add "Phonebook backend" steps 1-6

---
+++ part03/project01_phonebook_backend/index.js
@@ -0,0 +1,100 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+let people = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get("/info", (request, response) => {
+    response.send(`<div>Phonebook has info for ${people.length} people<br/><br/> ${new Date()}</div>`)
+})
+
+// get all
+app.get('/api/persons', (request, response) => {
+    response.json(people)
+})
+
+// get a person with the given id
+app.get("/api/persons/:id", (request, response) => {
+    const id = request.params.id
+    const person = people.find(person => person.id === id)
+
+    if (person) {
+
+        response.json(person)
+
+    } else {
+
+        response.status(404).end()
+    }
+})
+
+// post a new person's details to the phonebook
+app.post("/api/persons", (request, response) => {
+
+    const personData = request.body
+
+    if (!personData.name) {
+        return response.status(400).json({
+            error: "name missing"
+        })
+    }
+
+    if (!personData.number) {
+        return response.status(400).json({
+            error: "number missing"
+        })
+    }
+
+    if (people.find(person => person.name === personData.name)) {
+        // feels like maybe "409 conflict" makes sense here
+        return response.status(409).json({
+            error: "a person with the given name already exists in the phonebook"
+        })
+    }
+
+
+    personData.id = String(Math.ceil(Math.random()*Number.MAX_SAFE_INTEGER))
+    people = people.concat(personData)
+    response.json(personData)
+
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = request.params.id
+    people = people.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running at http://localhost:${PORT}/`)
+})
+
