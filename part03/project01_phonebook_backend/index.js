a2b7450692fa881967d05a036765c71785068954
TA <aaltopi@gmail.com>
2024-07-05 10:52:45 +0300

    Add "Phonebook Database" steps 3-6

--- part03/project01_phonebook_backend/index.js
+++ part03/project01_phonebook_backend/index.js
@@ -28,28 +28,28 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :p
 
 // ============================
 
-let people = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let people = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 
 app.get('/', (request, response) => {
@@ -57,7 +57,9 @@ app.get('/', (request, response) => {
 })
 
 app.get("/info", (request, response) => {
-    response.send(`<div>Phonebook has info for ${people.length} people<br/><br/> ${new Date()}</div>`)
+    Person.countDocuments().then(numDocs => {
+        response.send(`<div>Phonebook has info for ${numDocs} people<br/><br/> ${new Date()}</div>`)
+    })
 })
 
 // get all
@@ -69,24 +71,21 @@ app.get('/api/persons', (request, response) => {
     // of a new person updates the array and sends the new person to
     // the server as well, though not necessarily in that order).
     Person.find().then(result => {
-        people = result
         response.json(result)
     })
 })
 
 // get a person with the given id
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
-    const person = people.find(person => person.id === id)
 
-    if (person) {
-
-        response.json(person)
-
-    } else {
-
-        response.status(404).end()
-    }
+    Person.findById(id).then(result => {
+        if (result) {
+            response.json(result)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
 // post a new person's details to the phonebook
@@ -106,12 +105,22 @@ app.post("/api/persons", (request, response) => {
         })
     }
 
-    if (people.find(person => person.name === personData.name)) {
-        // feels like maybe "409 conflict" makes sense here
-        return response.status(409).json({
-            error: "a person with the given name already exists in the phonebook"
-        })
-    }
+
+    // Actually, the current state of the database should be kept
+    // locally consisted on the client side which already does the 
+    // check for whether the phonebook contains a name, so kind
+    // of unnecessary to fiddle with it here? In turn, it's kind of
+    // dumb anyway that you can't have the same name twice. I mean,
+    // people can have two or more phones/numbers, and preventing
+    // the addition of the same name twice is just extra work for the
+    // developer while restricting the user unnecessarily.
+
+    // if (Person.findOne({name: personData.name})) {
+    //     // feels like maybe "409 conflict" makes sense here
+    //     return response.status(409).json({
+    //         error: "a person with the given name already exists in the phonebook"
+    //     })
+    // }
 
     const person = new Person({
         name: personData.name,
@@ -119,18 +128,38 @@ app.post("/api/persons", (request, response) => {
     })
 
     person.save().then(savedPerson => {
-        people = people.concat(savedPerson)
         response.json(savedPerson)
     })
 
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
     const id = request.params.id
-    people = people.filter(note => note.id !== id)
+    const newNumber = request.body.number
 
-    response.status(204).end()
+    Person.findByIdAndUpdate(id, {number: newNumber}).then(result => {
+        response.status(204).end()
+    }).catch(error => next(error))
 })
+
+app.delete("/api/persons/:id", (request, response, next) => {
+    const id = request.params.id
+
+    Person.findByIdAndDelete(id).then(result => {
+        response.status(204).end()
+    }).catch(error => next(error))
+})
+
+function errorHandler(error, request, response, next)
+{
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    console.log("Error occured, giving catch-all 500")
+    response.status(500).end()
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
