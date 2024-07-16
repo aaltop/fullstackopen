93752b6abc3883f8148cb38be3d6094b64758362
TA <aaltopi@gmail.com>
2024-07-16 14:31:24 +0300

    Add "Blog List" step 2 (4.2)

--- part04/project01_blog_list/app.js
+++ part04/project01_blog_list/app.js
@@ -1,43 +1,25 @@
+const logger = require("./utils/logger")
+const blogsRouter = require("./routers/blogs")
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require("./utils/config")
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose.set("strictQuery", false)
 
 const mongoUrl = config.MONGODB_URL
 mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info("Connected to MongoDB")
+    })
+    .catch(error => {
+        logger.error("Error connecting to MongoDB:", error.message)
+    })
 
 app.use(cors())
-app.use(express.json())
+app.use("/api/blogs", blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
---
+++ part04/project01_blog_list/routers/blogs.js
@@ -0,0 +1,26 @@
+const Blog = require("../models/blog")
+
+const express = require("express")
+const blogsRouter = express.Router()
+
+
+blogsRouter.use(express.json())
+
+blogsRouter.get('/', (request, response) => {
+
+    Blog.find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog.save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = blogsRouter
