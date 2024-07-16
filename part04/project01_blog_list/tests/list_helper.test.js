84b0f64ac5180ee56733f9457bd988a1aff7f3c9
TA <aaltopi@gmail.com>
2024-07-16 21:08:18 +0300

    Add "Helper Functions and Unit Tests" step 4 (4.6)

--- part04/project01_blog_list/tests/list_helper.test.js
+++ part04/project01_blog_list/tests/list_helper.test.js
@@ -171,3 +171,82 @@ describe("favourite blog", () => {
     })
 
 })
+
+
+describe("most blogs", () => {
+    const blogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 12, // have as many as the other most liked
+            __v: 0
+        }  
+    ]
+
+    test("of zero length list is null", () => {
+        const most = listHelper.mostBlogs([])
+        assert.strictEqual(most, null)
+    })
+
+    test("of list of one is the author of the item with one blog", () => {
+        const most = listHelper.mostBlogs([blogs[0]])
+        const expected = {
+            author: "Michael Chan",
+            blogs: 1
+        }
+        assert.deepStrictEqual(most, expected)
+    })
+
+    test("of list of multiple is the author with most blogs and the number of blogs", () => {
+        const most = listHelper.mostBlogs(blogs)
+        const expected = {
+            author: "Robert C. Martin",
+            blogs: 3
+        }
+        assert.deepStrictEqual(most, expected)
+    })
+
+
+})
--- part04/project01_blog_list/utils/list_helper.js
+++ part04/project01_blog_list/utils/list_helper.js
@@ -1,6 +1,3 @@
-
-
-
 function dummy(blogs)
 {
     return 1
@@ -21,6 +18,27 @@ function favoriteBlog(blogs)
         : blogs.reduce((prev, cur) => prev.likes < cur.likes ? cur : prev)
 }
 
+function mostBlogs(blogs)
+{
+
+
+    if (blogs.length === 0) {
+        return null
+    }
+
+    // you know what? I AM just going to do it vanilla
+    const authorsCounter = new Map(blogs.map(blog => [blog.author, 0]))
+    blogs.forEach(blog => authorsCounter.set(blog.author, authorsCounter.get(blog.author)+1))
+    // because apparently iterators are still "Experimental"
+    const mostProlificAuthor = [...authorsCounter].reduce((prev, cur) => prev[1] < cur[1] ? cur : prev)
+    return {
+        author: mostProlificAuthor[0],
+        blogs: mostProlificAuthor[1]
+    }
+
+
+}
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs
 }
