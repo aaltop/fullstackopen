c55e36d954a8e6e1340d2f779e75a4cf007697a1
TA <aaltopi@gmail.com>
2024-07-16 17:59:42 +0300

    Add "Helper Functions and Unit Tests" step 2 (4.4)

--- part04/project01_blog_list/utils/list_helper.js
+++ part04/project01_blog_list/utils/list_helper.js
@@ -6,7 +6,14 @@ function dummy(blogs)
     return 1
 }
 
+function totalLikes(blogs)
+{
+    return (blogs.length === 0)
+        ?  0
+        : blogs.reduce((prev, cur) => cur.likes + prev, 0)
+}
+
 
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
