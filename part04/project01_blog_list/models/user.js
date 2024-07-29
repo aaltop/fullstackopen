523e3898e995186b203a057126e3de71d45c7502
TA <aaltopi@gmail.com>
2024-07-29 10:51:55 +0300

    Add partial "Blog List Expansions" step 3 (4.15)
    Added the User mongoose model.

---
+++ part04/project01_blog_list/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    name: String
+})
+
+userSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.passwordHash
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = {
+    User
+}
