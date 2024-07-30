30ebe9e23bd276c60fa2d4e4354cc96d6ffe7597
TA <aaltopi@gmail.com>
2024-07-30 11:07:31 +0300

    Change blogs toJSON to not return the version
    The __v thing, that is.

--- part04/project01_blog_list/models/blog.js
+++ part04/project01_blog_list/models/blog.js
@@ -18,6 +18,7 @@ blogSchema.set("toJSON", {
     transform: (doc, ret) => {
         ret.id = ret._id.toString()
         delete ret._id
+        delete ret.__v
         if (!Object.hasOwn(ret, "likes")) {
             ret.likes = 0
         }
