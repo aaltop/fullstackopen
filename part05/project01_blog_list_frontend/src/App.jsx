ec0bd8756ba946bad0698a6ea2282416dafc57b3
TA <aaltopi@gmail.com>
2024-08-14 19:40:00 +0300

    Add "Blog List Frontend" step 4 (5.4)
    Added better timeout stuff this time so it doesn't cancel out back-to-back
    notifications. In turn, it's still possible that a notification during
    login and then a quick successful login could result in the notification
    being still displayed once on the logged-in screen, which is slightly
    un-neat if nothing else. But it's not like it's a polished app anyway.

--- part05/project01_blog_list_frontend/src/App.jsx
+++ part05/project01_blog_list_frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import Blog from './components/Blog'
 import LoginForm from "./components/LoginForm"
 import BlogAddForm from "./components/BlogAddForm"
+import Notification from "./components/Notification"
 
 import blogService from './services/blogs'
 import loginService from "./services/login"
@@ -15,12 +16,14 @@ const App = () => {
     const [user, setUser] = useState(window.localStorage.getItem("user"))
     const [username, setUsername] = useState(window.localStorage.getItem("username"))
     const [name, setName] = useState(null)
+    const [notification, setNotification] = useState({text: null, timeoutId: null, success: true})
 
     async function fetchUser()
     {
         const userData = await userService.getUser(username, user)
         setBlogs(userData.blogs)
         setName(userData.name)
+
     }
 
     function logOut()
@@ -39,6 +42,27 @@ const App = () => {
         }
     }, [user])
 
+    function _setNotification(text, success, timeout)
+    {
+
+        if (notification.timeoutId) {
+            clearTimeout(notification.timeoutId)
+        }
+
+        const emptyNotification = {
+            text: null,
+            timeoutId: null,
+            success: true
+        }
+        setNotification(
+            {
+                text: text,
+                success: success,
+                timeoutId: setTimeout(() => setNotification(emptyNotification), timeout)
+            }
+        )
+    }
+
     async function attemptLogin(ev, username, password)
     {
         ev.preventDefault()
@@ -48,6 +72,8 @@ const App = () => {
             window.localStorage.setItem("user", token)
             window.localStorage.setItem("username", username)
             setUsername(username)
+        } else {
+            _setNotification("Invalid username or password", false, 5000)
         }
     }
 
@@ -55,20 +81,31 @@ const App = () => {
     {
         ev.preventDefault()
         const blog = {title, author, url}
-        const responseBlog = await blogService.addBlog(blog, user)
-        console.log(responseBlog)
-        setBlogs(blogs.concat(responseBlog))
+        try {
+            const responseBlog = await blogService.addBlog(blog, user)
+            console.log(responseBlog)
+            setBlogs(blogs.concat(responseBlog))
+            _setNotification(`Added new blog "${title}" from ${author}`, true, 5000)
+        } catch (e) {
+            if (e.name === "AxiosError" && e.response.status === 400) {
+                _setNotification(e.response.data.error, false, 5000)
+            }
+        }
     }
 
     if (null === user) {
         return (
-            <LoginForm submitAction={attemptLogin}/>
+            <>
+                <Notification text={notification.text} success={notification.success}/>
+                <LoginForm submitAction={attemptLogin}/>
+            </>
         )
     }
 
     return (
         
         <div>
+            <Notification text={notification.text} success={notification.success}/>
             <div>
             {name} is logged in
             <button type="button" onClick={logOut}>Log Out</button>
