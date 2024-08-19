feca6776016b0df43af9333ac179cb224ca34234
TA <aaltopi@gmail.com>
2024-08-19 13:40:00 +0300

    Add "Blog List Frontend" step 7 (5.7)
    Decided to also add a "styling" file, as that's probably something
    nice to have for a more cohesive style on the page (not that I'm necessary
    going to actually do much in the way of styling, but good to think ahead
    all the same).

--- part05/project01_blog_list_frontend/src/components/Blog.jsx
+++ part05/project01_blog_list_frontend/src/components/Blog.jsx
@@ -1,7 +1,61 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {lightMode as color} from "../style/color"
+
+import {useState} from "react"
+
+
+function Blog({ blog }) {
+
+    const [verbose, setVerbose] = useState(false)
+
+    const simpleStyle = {
+        outlineWidth: "thin",
+        outlineStyle: "solid",
+        outlineColor: color.blogOutline,
+        width: "fit-content",
+        margin: "10px",
+        padding: "5px"
+    }
+
+    const simple = (
+        <div style={simpleStyle}>
+            {blog.title} {blog.author} <button type="button" onClick={() => setVerbose(true)}>Show</button>
+        </div>
+    )
+
+    if (!verbose) {
+        return simple
+    }
+
+    const fullStyle = {
+        ...simpleStyle,
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gridGap: "20px",
+    }
+
+    const full = (
+        <div style={fullStyle}>
+            <div>
+                <div>
+                    {blog.title}
+                </div>
+                <div>
+                    {blog.url}
+                </div>
+                <div>
+                    likes {blog.likes} <button type="button">like</button>
+                </div>
+                <div>
+                    {blog.author}
+                </div>
+            </div>
+            <div>
+                <button type="button" onClick={() => setVerbose(false)}>Hide</button>
+            </div>
+        </div>
+    )
+
+    return full
+}
 
 export default Blog
---
+++ part05/project01_blog_list_frontend/src/style/color.js
@@ -0,0 +1,5 @@
+export const lightMode = {
+    success: "#111d8c",
+    failure: "#b5471b",
+    blogOutline: "#000000"
+}
