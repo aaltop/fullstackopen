cc94549b8f726630869153e9b19578d5df69512c
TA <aaltopi@gmail.com>
2024-09-03 13:05:01 +0300

    Add "Anecdotes and the Backend" step 1 (6.14)

---
+++ part06/project02_anecdotes/src/services/anecdotes.js
@@ -0,0 +1,17 @@
+import config from "./config"
+
+import axios from "axios"
+
+
+const baseUrl = config.baseUrlPrefix+"anecdotes"
+
+
+async function getAll()
+{
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+
+export default {
+    getAll
+}
---
+++ part06/project02_anecdotes/src/services/config.js
@@ -0,0 +1,5 @@
+const baseUrlPrefix = "http://localhost:3000/"
+
+export default {
+    baseUrlPrefix
+}
