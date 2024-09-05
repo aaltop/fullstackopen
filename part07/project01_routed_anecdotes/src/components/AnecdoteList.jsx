9f8d9614c7137320b24685ba61a93adffcca3f18
TA <aaltopi@gmail.com>
2024-09-05 14:30:50 +0300

    Add "Routed Anecdotes" step 1 (7.1)
    Routing should work now, but as I'm (I believe) using the newer method
    of writing React Routers, I'm having to move things around quite a
    bit. The main issue just now is that I need to actually share the
    the state somehow. I guess the react reducer stuff is good for that.

---
+++ part07/project01_routed_anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+
+
+
+export default function AnecdoteList() 
+{
+    const [anecdotes, setAnecdotes] = useState([
+        {
+          content: 'If it hurts, do it more often',
+          author: 'Jez Humble',
+          info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+          votes: 0,
+          id: 1
+        },
+        {
+          content: 'Premature optimization is the root of all evil',
+          author: 'Donald Knuth',
+          info: 'http://wiki.c2.com/?PrematureOptimization',
+          votes: 0,
+          id: 2
+        }
+    ])
+
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            <ul>
+                {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+            </ul>
+        </div>
+    )
+}
