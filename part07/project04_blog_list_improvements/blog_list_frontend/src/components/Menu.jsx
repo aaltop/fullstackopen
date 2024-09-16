6f1f6fb8438e6ae48a68e9fa33aa688218405d44
TA <aaltopi@gmail.com>
2024-09-16 18:18:37 +0300

    Style Navigation menu elements, "Styles" step 2 (7.21)

--- part07/project04_blog_list_improvements/blog_list_frontend/src/components/Menu.jsx
+++ part07/project04_blog_list_improvements/blog_list_frontend/src/components/Menu.jsx
@@ -1,22 +1,50 @@
-import { Link } from "../style/link"
+import { NavLink } from "react-router-dom"
 
-export default function Menu() {
-    const padding = {
-        paddingRight: 5,
+import styled from "styled-components"
+
+const MenuItem = styled(NavLink)`
+    font-size: larger;
+    text-decoration: none;
+    color: white;
+
+    &:visited {
+        color: white;
     }
 
+    &:hover {
+        // background-color: ${props =>
+            props.theme.bkgSoftHighlight};
+
+        text-decoration: underline;
+    }
+`
+
+function reactStyler({ isActive }) {
+    return isActive
+        ? {
+              borderLeft: "5px solid #d60",
+              backgroundColor: "#930",
+              paddingLeft: "5px",
+          }
+        : {}
+}
+
+const NavBar = styled.nav`
+    display: grid;
+    grid-template-columns: 1fr;
+    // border: 2px solid black;
+    width: 200px;
+`
+
+export default function Menu() {
     return (
-        <nav>
-            <div>
-                <Link to="/" style={padding}>
-                    Blogs
-                </Link>
-            </div>
-            <div>
-                <Link to="/users" style={padding}>
-                    Users
-                </Link>
-            </div>
-        </nav>
+        <NavBar>
+            <MenuItem to="/" style={reactStyler}>
+                Blogs
+            </MenuItem>
+            <MenuItem to="/users" style={reactStyler}>
+                Users
+            </MenuItem>
+        </NavBar>
     )
 }
--- part07/project04_blog_list_improvements/blog_list_frontend/src/style/theme.js
+++ part07/project04_blog_list_improvements/blog_list_frontend/src/style/theme.js
@@ -3,12 +3,17 @@ const link = {
     linkVisited: "#93f",
 }
 
+const color = {
+    backgroundColor: "#333",
+    bkgSoftHighlight: "#444",
+    highlight: "#d60",
+}
+
 const theme = {
     success: "#111d8c",
     failure: "#b5471b",
     blogOutline: "#000000",
-    backgroundColor: "#333",
-    bkgSoftHighlight: "#444",
+    ...color,
     ...link,
 }
 
