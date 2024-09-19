f04ab2acb05c325c78ea98a2b8a1c82034b9a71e
TA <aaltopi@gmail.com>
2024-09-19 17:09:36 +0300

    Add "Books of an author" (8.4)

--- part08/project01_library/index.js
+++ part08/project01_library/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]
+    allBooks(author: String): [Book!]
     allAuthors: [Author!]
   }
 `
@@ -125,7 +125,10 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (parent, args) => {
+            if (!args.author) return books
+            return books.filter(book => book.author === args.author)
+        },
         allAuthors: () => {
             const bookAuthors = books.map(book => book.author)
             return authors.map(author => {
