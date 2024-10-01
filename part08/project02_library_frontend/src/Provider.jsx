9c78af6466283565e5a96c638d090a64a23c8425
TA <aaltopi@gmail.com>
2024-10-01 15:17:25 +0300

    Add partial "Log in", authorization header (8.18)

--- part08/project02_library_frontend/src/Provider.jsx
+++ part08/project02_library_frontend/src/Provider.jsx
@@ -1,8 +1,29 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink
+} from '@apollo/client'
+
+import { setContext } from "@apollo/client/link/context"
 
 
-const apolloClient = new ApolloClient({
+const httpLink = createHttpLink({
     uri: "http://localhost:4000/",
+})
+  
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('userToken')
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      }
+    }
+})
+  
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache({
         typePolicies: {
             Author: {
