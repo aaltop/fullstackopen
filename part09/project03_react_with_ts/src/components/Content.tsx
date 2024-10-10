81591c1e231cd1d40733e859ee5f5ff5dd0bdaf5
TA <aaltopi@gmail.com>
2024-10-10 17:10:56 +0300

    Add "Exercise 9.16" (9.16)
    Bit of an ad hoc styling on the whole thing.
    
    While this certainly teaches about how one might use the literals
    for this kind trick, could I not also simply use conditional
    rendering, or indeed conditionality in general? The idea of
    defining a separate type for each slightly different object
    seems wacky. Seems a lot easier to just have some properties
    be optional then check for their existence, would be a lot easier
    with adhering to DRY this way.

--- part09/project03_react_with_ts/src/components/Content.tsx
+++ part09/project03_react_with_ts/src/components/Content.tsx
@@ -1,7 +1,98 @@
+import { assertNever } from "../typing/utils";
 
-interface CoursePart {
-    name: string,
-    exerciseCount: number
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+}
+
+interface CoursePartBaseDescript extends CoursePartBase {
+    description: string;
+}
+
+const coursePartBasicKind = "basic";
+interface CoursePartBasic extends CoursePartBaseDescript {
+    kind: typeof coursePartBasicKind
+}
+
+const coursePartBackgroundKind = "background"
+interface CoursePartBackground extends CoursePartBaseDescript {
+    backgroundMaterial: string;
+    kind: typeof coursePartBackgroundKind
+}
+
+const coursePartGroupKind = "group"
+interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: typeof coursePartGroupKind
+}
+
+const CoursePartRequirementsKind = "special"
+interface CoursePartRequirements extends CoursePartBaseDescript {
+    requirements: string[];
+    kind: typeof CoursePartRequirementsKind
+}
+
+
+export type CoursePart = CoursePartBasic
+| CoursePartGroup 
+| CoursePartBackground
+| CoursePartRequirements;
+
+function Part({ coursePart }: { coursePart: CoursePart})
+{
+
+    const header = (
+        <div style={{paddingTop: "1em"}}>
+            <b>{coursePart.name} {coursePart.exerciseCount}</b>
+        </div>
+    )
+
+    function Description({ desc }: { desc: CoursePartBaseDescript["description"]})
+    {
+        return (
+            <i>{desc}</i>
+        )
+    }
+
+    switch (coursePart.kind) {
+        case (coursePartBasicKind): {
+            return (
+                <div>
+                    {header}
+                    <Description desc={coursePart.description} />
+                </div>
+            )
+        }
+        case (coursePartBackgroundKind): {
+            return (
+                <div>
+                    {header}
+                    <Description desc={coursePart.description} /><br></br>
+                    {`Background material:`} <a href={coursePart.backgroundMaterial}>{coursePart.backgroundMaterial}</a>
+                </div>
+            )
+        }
+        case (coursePartGroupKind): {
+            return (
+                <div>
+                    {header}
+                    {`Group projects: ${coursePart.groupProjectCount}`}
+                </div>
+            )
+        }
+        case (CoursePartRequirementsKind): {
+            return (
+                <div>
+                    {header}
+                    <Description desc={coursePart.description} /><br></br>
+                    {`Requirements: ${coursePart.requirements.join(", ")}`}
+                </div>
+            )
+        }
+        default: {
+            assertNever(coursePart);
+        }
+    };
 }
 
 interface ContentProps {
@@ -12,13 +103,7 @@ export default function Content({ courseParts }: ContentProps)
 {
     return (
         <>
-            {courseParts.map(coursePart => {
-                return (
-                    <p key={coursePart.name}>
-                        {coursePart.name} {coursePart.exerciseCount}
-                    </p>
-                )
-            })}
+            {courseParts.map(coursePart => <Part key={coursePart.name} coursePart={coursePart} />)}
         </>
     );
 }
---
+++ part09/project03_react_with_ts/src/typing/utils.ts
@@ -0,0 +1,8 @@
+
+
+
+export function assertNever(value: never): never {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+};
