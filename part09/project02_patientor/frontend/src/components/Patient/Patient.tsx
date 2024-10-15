b3af7ec6cf82aa609fa8e4c170293fbdb1e44170
TA <aaltopi@gmail.com>
2024-10-15 19:37:47 +0300

    Add "Patientor, step 4" (9.24)

--- part09/project02_patientor/frontend/src/components/Patient/Patient.tsx
+++ part09/project02_patientor/frontend/src/components/Patient/Patient.tsx
@@ -1,4 +1,4 @@
-import { Patient as PatientType } from "../../types";
+import { Patient as PatientType, Entry as EntryType } from "../../types";
 import patientService from "../../services/patients";
 
 import { useParams } from "react-router-dom";
@@ -8,6 +8,24 @@ import { Container, Typography } from "@mui/material";
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from "@mui/icons-material/Female";
 
+
+function Entry({ entry }: { entry: EntryType })
+{
+
+    let codes = entry.diagnosisCodes?.join(", ") ?? null;
+    if (codes) codes = `Diagnosis codes: ${codes}`;
+
+    return (
+        <div style={{
+            borderBlock: "1px solid black",
+            paddingBlock: "1em",
+            marginBlock: "1px"
+        }}>
+            {`${entry.date}: `} <i>{entry.description}</i><br></br>
+            {codes}
+        </div>
+    );
+}
 
 
 export default function Patient()
@@ -37,6 +55,8 @@ export default function Patient()
             <Typography variant="h4">{patient.name} {genderIcon}</Typography>
             {`ssn: ${patient.ssn ?? "unknown"}`} <br></br>
             {`occupation: ${patient.occupation}`}
+            <Typography variant="h5">Entries</Typography>
+            {patient.entries.map(entry => <Entry key={entry.id} entry={entry} />)}
         </Container>
     );
 
--- part09/project02_patientor/frontend/src/types.ts
+++ part09/project02_patientor/frontend/src/types.ts
@@ -2,25 +2,17 @@ import parsers from "./parsers";
 
 import { z } from "zod";
 
-export interface Diagnosis {
-    code: string,
-    name: string,
-    latin?: string
-}
+export type Diagnosis = z.infer<typeof parsers.Diagnosis>;
 
 export type Patient = z.infer<typeof parsers.Patient>;
 export type NonSensitivePatient = z.infer<typeof parsers.NonSensitivePatient>;
-export type PatientFormValues = z.infer<typeof parsers.NewPatient>;
+export type NewPatient = z.infer<typeof parsers.NewPatient>;
+
+export type Entry = z.infer<typeof parsers.EntryUnion>;
 
 export type ErrorResponse = {
     error: string
 };
-
-export interface Diagnosis {
-    code: string;
-    name: string;
-    latin?: string;
-}
 
 export enum Gender {
     Male = "male",
